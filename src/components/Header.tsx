69a19761f7796f753d9c92b5bad06ef1d9e7ed70
Alif Hakimi <alifhakimiazwan@gmail.com>
2025-03-19 22:22:28 +0800

    design landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,14 +56,16 @@ const Header = async () => {
         </SheetContent>
       </Sheet>
       <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
-        <h2 className="px-5 font-extrabold font-telegraf">RateMyCitra</h2>
+        <h2 className="px-5 font-extrabold text-xl font-telegraf">
+          RateMyCitra
+        </h2>
       </Link>
       <NavigationMenu className="hidden lg:flex">
         <NavigationMenuList>
           <NavigationMenuLink asChild>
             <Link
               href="/explore"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
               Explore
@@ -72,7 +74,7 @@ const Header = async () => {
           <NavigationMenuLink asChild>
             <Link
               href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
               About
@@ -82,7 +84,7 @@ const Header = async () => {
           <NavigationMenuLink asChild>
             <Link
               href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
               prefetch={false}
             >
               Contact
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 
 export default async function Hero() {
   return (
-    <div className="px-7">
+    <div
+      className="px-7 min-h-screen"
+      style={{
+        backgroundImage: "url('/gradient.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       {" "}
       <Header />
       {/* Hero */}
@@ -14,24 +21,23 @@ export default async function Hero() {
         <div
           aria-hidden="true"
           className="flex absolute -top-96 start-1/2 transform -translate-x-1/2"
-        >
-          <div className="bg-gradient-to-r from-background/50 to-background blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]" />
-          <div className="bg-gradient-to-tl blur-3xl w-[90rem] h-[50rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] from-primary-foreground via-primary-foreground to-background" />
-        </div>
+        ></div>
         {/* End Gradients */}
-        <div className="relative z-10">
+        <div className="relative z-10 ">
           <div className="container py-4 lg:py-16">
             <div className="max-w-2xl text-center mx-auto">
-              <p className="">Anonymously review Citra subjects using</p>
+              <p className="font-telegraf text-white font-bold text-lg">
+                Anonymously review Citra subjects using
+              </p>
               {/* Title */}
               <div className="mt-5 max-w-2xl">
-                <h1 className="scroll-m-20 text-4xl font-extrabold font-telegraf tracking-tight lg:text-5xl">
+                <h1 className="scroll-m-20 text-4xl font-extrabold font-telegraf tracking-tight  text-white md:text-9xl">
                   RateMyCitra
                 </h1>
               </div>
               {/* End Title */}
               <div className="mt-5 max-w-3xl">
-                <p className="text-md text-muted-foreground ">
+                <p className="text-md text-muted-foreground text-white">
                   Discover the best elective subjects Citra with insights from
                   past students and alumni. Explore ratings, reviews, and
                   feedback to make informed choices for your academic journey.
